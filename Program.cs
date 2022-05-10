cf431afe659e1d4e0216475e46f0d732e4ecf2b3
PsykoDev <45910905+PsykoDev@users.noreply.github.com>
2022-05-10 19:32:17 +0200

    Chocolatey update

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace BackUpDesktop
         {
             Console.Write("1: Backup\n" +
                           "2: Winget install\n" +
+                          "3: Chocolatey install\n" +
                           "> ");
 
             int sw = int.Parse(Console.ReadLine());
@@ -26,13 +27,21 @@ namespace BackUpDesktop
                     StartBackupAsync();
                     break;
                 case 2:
-                    StartWinget();
+                    StartWingetAsync();
                     break;
+                case 3:
+                    StartChocoInstallAsync();
+                        break;
             }
         }
 
+        // choco install <path-to-exported-file> -y
+        // choco upgrade chocolatey -y
+
+        // Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iwr https://community.chocolatey.org/install.ps1 -UseBasicParsing | iex
+
         // Winget install Process
-        private static void StartWinget()
+        private static async Task StartWingetAsync()
         {
             Console.WriteLine("Select foler where is saved Winget.json");
 
@@ -49,12 +58,8 @@ namespace BackUpDesktop
             Console.WriteLine("Winget.json file found ! {0}", files[0]);
             Console.WriteLine("Please wait!");
             Thread.Sleep(3000);
-            Process process = new Process();
-            ProcessStartInfo startInfo = new ProcessStartInfo();
-            startInfo.FileName = "cmd.exe";
-            startInfo.Arguments = $"/C winget import -i {files[0]} --accept-source-agreements --accept-package-agreements";
-            process.StartInfo = startInfo;
-            process.Start();
+
+            await Exec("cmd.exe", "winget import -i {files[0]} --accept-source-agreements --accept-package-agreements");
             // winget install -e -h --accept-source-agreements --accept-package-agreements --id $app.name
         }
 
@@ -77,7 +82,7 @@ namespace BackUpDesktop
             var ProgConf = true;
             while (ProgConf)
             {
-                Console.Write("Confirmation to backup all program name as TXT file: ");
+                Console.Write("Confirmation to backup all program name as TXT file (Y)es | (N)o: ");
                 string answer = Console.ReadLine();
                 if (answer.ToLower() == "y")
                 {
@@ -88,25 +93,45 @@ namespace BackUpDesktop
                     InstallApp.FindInstalls(RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64), keys);
                     InstallApp.FindInstalls(RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry64), keys);
                 }
+                else
+                {
+                    break;
+                }
             }
 
             var WinGetConf = true;
             while (WinGetConf)
             {
-                Console.Write("Confirmation to create a Winget file: ");
+                Console.Write("Confirmation to create a Winget export file (Y)es | (N)o: ");
                 string answer = Console.ReadLine();
                 if (answer.ToLower() == "y")
                 {
                     WinGetConf = false;
                     // Make a winGet export 
-                    Process process = new Process();
-                    ProcessStartInfo startInfo = new ProcessStartInfo();
-                    startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-                    startInfo.FileName = "cmd.exe";
-                    startInfo.Arguments = $"/C winget export -o '{BackUpPath}\\WinGet.json'";
-                    process.StartInfo = startInfo;
-                    startInfo.CreateNoWindow = true;
-                    process.Start();
+                    await Exec("cmd.exe", $"winget export -o {BackUpPath}\\WinGet.json");
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            var ChocoConf = true;
+            while (ChocoConf)
+            {
+                Console.Write("Confirmation to create a Chocolatey export file (Y)es | (N)o: ");
+                string answer = Console.ReadLine();
+                if (answer.ToLower() == "y")
+                {
+                    ChocoConf = false;
+                    // Chocolatey install
+                    await Exec("powershell.exe", "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iwr https://community.chocolatey.org/install.ps1 -UseBasicParsing | iex");
+                    // Make a Chocolatey export 
+                    await Exec("cmd.exe", $"choco export --output-file-path=\"'{BackUpPath}\\ChocolateyBackup.config'\"");
+                }
+                else
+                {
+                    break;
                 }
             }
 
@@ -114,25 +139,62 @@ namespace BackUpDesktop
             var conf = true;
             while (conf)
             {
-                Console.Write("Confirmation before starting type Y to start or close this program: ");
+                Console.Write("Confirmation before starting type Y to start or close this program (Y)es | (N)o: ");
                 string answer = Console.ReadLine();
                 if (answer.ToLower() == "y")
                 {
                     conf = false;
+                    List<string> list = KnownFolders.GetPath();
+                    string[] str = list.ToArray();
+                    await DataCopy.FFCopy(str);
+
+                    Console.WriteLine("Press any key to Exit.");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    break;
                 }
             }
-
-            // Staring Detection / Copy
-            if (!conf)
-            {
-                List<string> list = KnownFolders.GetPath();
-                string[] str = list.ToArray();
-                await DataCopy.FFCopy(str);
-
-                Console.WriteLine("Press any key to Exit.");
-                Console.ReadKey();
-            }
         }
+
+        private static async Task StartChocoInstallAsync()
+        {
+            Console.WriteLine("Select foler where is saved packages.config");
+
+            // Select Back up folder
+            var dlg = new FolderPicker();
+            dlg.InputPath = @"c:";
+            dlg.Title = "Auto Chocolatey";
+            if (dlg.ShowDialog(owner: IntPtr.Zero) == true)
+            {
+                BackUpPath = dlg.ResultPath;
+            }
+
+            string[] files = Directory.GetFiles(BackUpPath, "*.config", SearchOption.TopDirectoryOnly);
+            Console.WriteLine("ChocolateyBackup.config file found ! {0}", files[0]);
+            Console.WriteLine("Please wait!");
+            Thread.Sleep(3000);
+            await Exec("powershell.exe", "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iwr https://community.chocolatey.org/install.ps1 -UseBasicParsing | iex");
+
+            await Exec("cmd.exe", $"choco install {BackUpPath}\\packages.config");
+        }
+
+        private static Task Exec(string filename, string cmd)
+        {
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.WindowStyle = ProcessWindowStyle.Normal;
+            startInfo.FileName = filename;
+            startInfo.Arguments = $"/C " + cmd;
+            process.StartInfo = startInfo;
+            //startInfo.CreateNoWindow = true;
+            process.Start();
+            process.WaitForExit();
+            process.Close();
+            return Task.CompletedTask;
+        }
+
 
     }
 }
